2e97c60af892efc9457d410dadef225832af6874
belaymit <2belamit@gmail.com>
2024-09-08 22:36:11 +0300

    Added tech section

---
+++ src/components/Tech.tsx
@@ -0,0 +1,31 @@
+import { motion } from "framer-motion";
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
+import { styles } from "../style";
+import { textVariant } from "../utils/motion";
+
+const Tech = () => {
+  return (
+    <>
+     <motion.div variants={textVariant(0.2)}>
+        <p className={`${styles.sectionSubText} text-center`}>
+          Technologies I've worked with
+        </p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>
+          TechStack
+        </h2>
+      </motion.div>
+      
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((technology) => (
+        <div className='w-28 h-28' key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Tech, "");
